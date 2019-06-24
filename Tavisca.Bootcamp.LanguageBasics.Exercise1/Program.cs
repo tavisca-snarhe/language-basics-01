0bda4a13a040ca507181811c51903a626996913c
Venom <saurabhnarhe123@gmail.com>
2019-06-24 10:16:53 +0530

    formatted

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -21,32 +21,38 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         }
 
         public static int FindDigit(string equation)
-        {  
+        {
             string num1 = equation.Split("*")[0];
             string num2 = equation.Split("*")[1].Split("=")[0];
-            string ans  = equation.Split("=")[1];
+            string ans = equation.Split("=")[1];
             string res;
             int index;
 
-            if(num1.Contains("?")) {
-                if(Int32.Parse(ans) % Int32.Parse(num2) == 0) {
+            if (num1.Contains("?"))
+            {
+                if (Int32.Parse(ans) % Int32.Parse(num2) == 0)
+                {
                     res = (Int32.Parse(ans) / Int32.Parse(num2)).ToString();
                     index = num1.IndexOf("?");
                     if (string.Equals(res, num1.Replace('?', res[index])))
                         return Int32.Parse(res[index].ToString());
                 }
-                
-            } else if(num2.Contains("?")) {
-                if (Int32.Parse(ans) % Int32.Parse(num1) == 0) {
+            }
+            else if (num2.Contains("?"))
+            {
+                if (Int32.Parse(ans) % Int32.Parse(num1) == 0)
+                {
                     res = (Int32.Parse(ans) / Int32.Parse(num1)).ToString();
                     index = num2.IndexOf("?");
                     if (string.Equals(res, num2.Replace('?', res[index])))
                         return Int32.Parse(res[index].ToString());
                 }
-            } else if(ans.Contains("?")) {
-                res = (Int32.Parse(num1)*Int32.Parse(num2)).ToString(); 
+            }
+            else if (ans.Contains("?"))
+            {
+                res = (Int32.Parse(num1) * Int32.Parse(num2)).ToString();
                 index = ans.IndexOf("?");
-                if(string.Equals(res, ans.Replace('?', res[index]))) 
+                if (string.Equals(res, ans.Replace('?', res[index])))
                     return Int32.Parse(res[index].ToString());
             }
 
